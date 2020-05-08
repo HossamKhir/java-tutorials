3382fb7b318fef13321fda8410d058f0e46b64e4
HossamKhir <hossam.khir1@outlook.com>
2020-05-08 13:47:29 +0200

    JComboBox: done

---
+++ src/tutorial/swing/combo/box/FormEvent.java
@@ -0,0 +1,105 @@
+package tutorial.swing.combo.box;
+
+import java.util.EventObject;
+
+public class FormEvent extends EventObject {
+
+    // <editor-fold desc="fields">
+    private int ageCategory;
+    private String name;
+    private String occupation;
+    private String empCat;
+    // </editor-fold>
+    // <editor-fold desc="constructors">
+
+    /**
+     * @param source
+     */
+    public FormEvent(Object source) {
+        super(source);
+    }
+
+    /**
+     * @param source
+     * @param objectList
+     */
+    public FormEvent(Object source, Object... objectList) {
+        super(source);
+        switch (objectList.length) {
+            case 4:
+                this.empCat = (String) objectList[3];
+            case 3:
+                this.ageCategory = (int) objectList[2];
+            case 2:
+                this.occupation = (String) objectList[1];
+            case 1:
+                this.name = (String) objectList[0];
+                break;
+            default:
+
+        }
+    }
+    // </editor-fold>
+    // <editor-fold desc="main">
+    // </editor-fold>
+    // <editor-fold desc="methods">
+
+    /**
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return
+     */
+    public String getOccupation() {
+        return occupation;
+    }
+
+    /**
+     * @param occupation
+     */
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    /**
+     * @return
+     */
+    public int getAgeCategory() {
+        return ageCategory;
+    }
+
+    /**
+     * @param ageCategory
+     */
+    public void setAgeCategory(int ageCategory) {
+        this.ageCategory = ageCategory;
+    }
+
+    /**
+     * @return
+     */
+    public String getEmpCat() {
+        return empCat;
+    }
+
+    /**
+     * @param empCat
+     */
+    public void setEmpCat(String empCat) {
+        this.empCat = empCat;
+    }
+
+
+    // </editor-fold>
+}
